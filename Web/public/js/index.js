ad6c3b366ff3a3fdb6ea4568de366100e7600a0d
Thomas Lebrun <thomas.lbrn76@gmail.com>
2017-11-07 13:52:36 +0100

    Get key from server

--- Web/public/js/index.js
+++ Web/public/js/index.js
@@ -3,3 +3,22 @@ require(['socket.io/socket.io.js']);
 
 
 var socket = io.connect('localhost:8080');
+
+//GERER UNE MACHINE A ETAT
+
+//CREER FONCTION SWITCH SCREEN POUR PASSER D'UN ECRAN A L'AUTRE
+
+function StartGame(){
+  socket.emit('start_game');
+  socket.on('room_opened', function(data){
+      //CALCULER UNE CLE
+      console.log("Salon ouvert avec la clé : " + data['key_code']);
+
+      var key = data['key_code'];
+  });
+}
+
+function CancelGame(){
+  socket.emit('cancel_game');
+  socket.off('room_opened');
+}
